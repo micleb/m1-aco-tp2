2e1f151cbee87cf6ad11995e88928c8445dd3fca
BhzErlb <erwan.leflem@etudiant.univ-rennes1.fr>
2017-11-27 12:14:48 +0100

    Refactoring des tests
    de la fonctionnalité défaire/refaire pour une meilleure lisibilité.

--- test/fr/istic/m1/aco/miniediteur/v1/memento/CommandsHistoricTest.java
+++ test/fr/istic/m1/aco/miniediteur/v1/memento/CommandsHistoricTest.java
@@ -77,9 +77,11 @@ public class CommandsHistoricTest {
 		assertEquals("1234567892345ABCDEF", m.getContent());
 	}
 	
-	
+	final String stage1CollerMultiple = "1234567892345ABCDEF";
+	final String stage2CollerMultiple = "2345234567892345ABCDEF"; 
+	final String stage3CollerMultiple = "2345234567892345ABCD2345EF";
 	@Test
-	public void testCollerMultiple() {
+	public void testCollerMultipleUndo() {
 		Selection s = new SelectionImpl(1, 4); 
 		Command select = this.createSelectCmd(s);
 		select.executer();
@@ -88,18 +90,16 @@ public class CommandsHistoricTest {
 		assertEquals("2345", m.getPresspapierContent());
 		
 		//ETAPE 1 : A partir de l'indice 9, on fait une selection de taille 0, on colle donc "2345" à la suite de "9" sans suppression.
-		final String stage1 = "1234567892345ABCDEF";
 		s = new SelectionImpl(9, 0); 
 		Command sel = createSelectCmd(s);
 		sel.executer();
 		cmds.registerCommand(sel);
 		Command paste = new Coller(m);
 		paste.executer();
-		assertEquals(stage1, m.getContent());
+		assertEquals(stage1CollerMultiple, m.getContent());
 		cmds.registerCommand(paste);
 		
 		//ETAPE 2 : A partir de l'indice 0, on fait une selection de taille 1, on colle donc "2345" en supprimant "1"
-		final String stage2 = "2345234567892345ABCDEF"; 
 		s = new SelectionImpl(0, 1);
 		sel = createSelectCmd(s);
 		sel.executer();
@@ -107,68 +107,78 @@ public class CommandsHistoricTest {
 				
 		paste = new Coller(m);
 		paste.executer();
-		assertEquals(stage2, m.getContent());
+		assertEquals(stage2CollerMultiple, m.getContent());
 		cmds.registerCommand(paste);
 		
 		//ETAPE 3 :A partir de l'indice 20, on fait une selection de taille 0, on colle donc "2345" à la suite de "D" sans suppression.
-		final String stage3 = "2345234567892345ABCD2345EF";
 		s = new SelectionImpl(20, 0); 
 		sel = createSelectCmd(s);
 		sel.executer();
 		cmds.registerCommand(sel);
 		paste = new Coller(m);
 		paste.executer();
-		assertEquals(stage3, m.getContent());
+		assertEquals(stage3CollerMultiple, m.getContent());
 		cmds.registerCommand(paste);
 		
 		
 		//On annule l'étape 3, on s'attend donc à retrouver le même contenu qu'a la fin de l'étape 2.
 		cmds.undo();
-		assertEquals(stage2, m.getContent());
+		assertEquals(stage2CollerMultiple, m.getContent());
 		//On annule l'étape 2, on s'attend donc à retrouver le même contenu qu'a la fin de l'étape 1.
 		cmds.undo();
-		assertEquals(stage1, m.getContent());
+		assertEquals(stage1CollerMultiple, m.getContent());
 		//On annule l'étape 1, on s'attend donc à retrouver le contenu initial.
 		cmds.undo();
 		assertEquals(INITIAL_CONTENT, m.getContent());
-		
-		
-		//Après avoir restoré le contenu initial, on refait l'étape 1 (equivalent de CTRL+Y),
-		//donc on doit se retrouver avec le même contenu qu'a la fin de l'étape 1. 
-		cmds.redo();
-		assertEquals(stage1, m.getContent());
-		//Restoration de registerCommand(cmd);	la commande suivante, donc on retrouve le conteu de l'étape 2.
-		cmds.redo();
-		assertEquals(stage2, m.getContent());
-		//Puisqu'on a fait 3 redo après nos 3 undo, c'est comme si on avait jamais fait d'undo.
-		//Donc on retrouve à la situation de l'étape 3.
-		cmds.redo();
-		assertEquals(stage3, m.getContent());	
-		
-		//Quelques CTRL-Z, CTRL-Y suivant la même logique.
-		cmds.undo();
-		assertEquals(stage2, m.getContent());
-		cmds.undo();
-		assertEquals(stage1, m.getContent());
-		cmds.redo();
-		assertEquals(stage2, m.getContent());
-		cmds.undo();
-		assertEquals(stage1, m.getContent());
-		cmds.redo();
-		assertEquals(stage2, m.getContent());
-		cmds.redo();
-		assertEquals(stage3, m.getContent());
 	}
 	
 	@Test
-	public void testCouperMultiple() {
+	public void testCollerMultipleRedo() {
+		testCollerMultipleUndo();
+		//Après avoir restoré le contenu initial, on refait l'étape 1 (equivalent de CTRL+Y),
+		//donc on doit se retrouver avec le même contenu qu'a la fin de l'étape 1. 
+		cmds.redo();
+		assertEquals(stage1CollerMultiple, m.getContent());
+		//Restoration de registerCommand(cmd);	la commande suivante, donc on retrouve le conteu de l'étape 2.
+		cmds.redo();
+		assertEquals(stage2CollerMultiple, m.getContent());
+		//Puisqu'on a fait 3 redo après nos 3 undo, c'est comme si on avait jamais fait d'undo.
+		//Donc on retrouve à la situation de l'étape 3.
+		cmds.redo();
+		assertEquals(stage3CollerMultiple, m.getContent());	
+	}
+	
+	@Test 
+	public void testCollerMultipleUndoRedo() {
+		testCollerMultipleRedo();
+		//Quelques CTRL-Z, CTRL-Y suivant la même logique.
+		cmds.undo();
+		assertEquals(stage2CollerMultiple, m.getContent());
+		cmds.undo();
+		assertEquals(stage1CollerMultiple, m.getContent());
+		cmds.redo();
+		assertEquals(stage2CollerMultiple, m.getContent());
+		cmds.undo();
+		assertEquals(stage1CollerMultiple, m.getContent());
+		cmds.redo();
+		assertEquals(stage2CollerMultiple, m.getContent());
+		cmds.redo();
+		assertEquals(stage3CollerMultiple, m.getContent());
+	}
+	
+	final String stage1CouperMultiple = "16789ABCDEF";
+	final String stage2CouperMultiple = "16789ABCD2345EF";
+	final String stage3CouperMultiple = "23456789ABCD2345EF"; 
+	final String stage4CouperMultiple = "23456789ABCD2345EF2345";
+	
+	@Test
+	public void testCouperMultipleUndo() {
 		Selection s = new SelectionImpl(1, 4); 
 		Command select = this.createSelectCmd(s);
 		select.executer();
 		cmds.registerCommand(select);
 		
 		//ETAPE 1 : On execute un couper des caractères 1 à 4.
-		final String stage1 = "16789ABCDEF";
 		Command mv = new Couper(m);
 		//mv.executer();
 		cmds.registerCommand(mv);
@@ -177,18 +187,16 @@ public class CommandsHistoricTest {
 		assertEquals("16789ABCDEF", m.getContent());
 		
 		//ETAPE 2 : A partir de l'indice 9, on fait une selection de taille 0, on colle donc "2345" à la suite de "D" sans suppression.
-		final String stage2 = "16789ABCD2345EF";
 		s = new SelectionImpl(9, 0); 
 		Command sel = createSelectCmd(s);
 		sel.executer();
 		cmds.registerCommand(sel);
 		Command paste = new Coller(m);
 		paste.executer();
-		assertEquals(stage2, m.getContent());
+		assertEquals(stage2CouperMultiple, m.getContent());
 		cmds.registerCommand(paste);
 		
-		//ETAPE 3 : A partir de l'indice 0, on fait une selection de taille 1, on colle donc "2345" en supprimant "1"
-		final String stage3 = "23456789ABCD2345EF"; 
+		//ETAPE 3 : A partir de l'indice 0, on fait une selection de taille 1, on colle donc "2345" en supprimant "1".
 		s = new SelectionImpl(0, 1);
 		sel = createSelectCmd(s);
 		sel.executer();
@@ -196,50 +204,52 @@ public class CommandsHistoricTest {
 				
 		paste = new Coller(m);
 		paste.executer();
-		assertEquals(stage3, m.getContent());
+		assertEquals(stage3CouperMultiple, m.getContent());
 		cmds.registerCommand(paste);
 				
 		//ETAPE 4 :A partir de l'indice 20, on fait une selection de taille 0, on colle donc "2345" à la suite de "D" sans suppression.
-		final String stage4 = "23456789ABCD2345EF2345";
 		s = new SelectionImpl(18, 0); 
 		sel = createSelectCmd(s);
 		sel.executer();
 		cmds.registerCommand(sel);
 		paste = new Coller(m);
 		paste.executer();
-		assertEquals(stage4, m.getContent());
+		assertEquals(stage4CouperMultiple, m.getContent());
 		cmds.registerCommand(paste);
 		
 		
 		//On annule l'étape 4, on s'attend donc à retrouver le même contenu qu'a la fin de l'étape 3.
 		cmds.undo();
-		assertEquals(stage3, m.getContent());
+		assertEquals(stage3CouperMultiple, m.getContent());
 		//On annule l'étape 3, on s'attend donc à retrouver le même contenu qu'a la fin de l'étape 2.
 		cmds.undo();
-		assertEquals(stage2, m.getContent());
+		assertEquals(stage2CouperMultiple, m.getContent());
 		//On annule l'étape 2, on s'attend donc à retrouver le même contenu qu'a la fin de l'étape 1.
 		cmds.undo();
-		assertEquals(stage1, m.getContent());
+		assertEquals(stage1CouperMultiple, m.getContent());
 		//On annule l'étape 1, on s'attend donc à retrouver le contenu initial.
 		cmds.undo();
 		assertEquals(INITIAL_CONTENT, m.getContent());
-		
+	}
+	
+	@Test
+	public void testCouperMultipleRedo() {
+		testCouperMultipleUndo();
 		//Après avoir restoré le contenu initial, on refait l'étape 1 (equivalent de CTRL+Y),
 		//donc on doit se retrouver avec le même contenu qu'a la fin de l'étape 1. 
 		cmds.redo();
-		assertEquals(stage1, m.getContent());
+		assertEquals(stage1CouperMultiple, m.getContent());
 		//Restoration de la commande suivante, donc on retrouve le conteu de l'étape 2.
 		cmds.redo();
-		assertEquals(stage2, m.getContent());
+		assertEquals(stage2CouperMultiple, m.getContent());
 		//Puisqu'on a fait 3 redo après nos 3 undo, c'est comme si on avait jamais fait d'undo.
 		//Donc on retrouve à la situation de l'étape 3.
 		cmds.redo();
-		assertEquals(stage3, m.getContent());
+		assertEquals(stage3CouperMultiple, m.getContent());
 		
 		cmds.redo();
-		assertEquals(stage4, m.getContent());
+		assertEquals(stage4CouperMultiple, m.getContent());
 	}
-	
 	@Test
 	public void testUndoInserer(){
 		Selection s = new SelectionImpl(9, 3); 
